1dec89ed005d8aaf52c27adf88c67022f6a20e26
Khyati Gupta <khyatig0206@gmail.com>
2024-08-20 11:56:21 +0530

    first commit

--- billionaries.py
+++ billionaries.py
@@ -18,7 +18,7 @@ def req(datadome_cookie, vwo):
     payload = ""
     
     headers = {
-        # 'Cookie': f'datadome={datadome_cookie}; VWO={vwo};',
+        'Cookie': f'datadome={datadome_cookie}; VWO={vwo};',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
         'Referer': 'https://www.forbes.com/billionaires/'
     }
